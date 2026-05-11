805b6ba67de3e8af05956699b0fd1b06b46e5014
AJCobraa <anupamjose99@gmail.com>
2026-05-12 01:39:44 +0530

    update

--- backend/scratch/check_fixed_cost.py
+++
@@ -1,7 +0,0 @@
-
-import sys
-import os
-sys.path.append(os.path.join(os.getcwd(), "backend"))
-from core.constants import FIXED_COST
-print(f"FIXED_COST: {FIXED_COST}")
-print(f"parse_resume_pdf + embed_resume: {FIXED_COST['parse_resume_pdf'] + FIXED_COST['embed_resume']}")
